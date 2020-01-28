edb79a6b2bfde4ed12bab5837b5a17df37a920b2
DayanePeres <dayane.fernandaperes@gmail.com>
2020-01-28 17:50:34 -0300

    Iniciando teste de integração

---
+++ UnitTest/Configuration/BaseIntegrationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Configuration
+{
+    public abstract class BaseIntegrationTest
+    {
+        protected readonly TestServer testServer;
+        protected readonly HttpClient Cliente;
+        protected readonly DataContext TestDataContext;
+
+        protected BaseTestFixture Fixture { get; set; }
+    }
+}
